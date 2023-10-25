59ba6048724b6bbc413577af94447ff59ac1cc66
uslstenn <shurygin0171@gmail.com>
2023-10-25 13:56:39 +0300

    [ir_impl] ir refactoring
    new intrusive list was implemented
    intrusive list gtest inited

---
+++ include/intrusive_list/ilist_iterator.hh
@@ -0,0 +1,100 @@
+#pragma once
+
+#include <cstddef>
+#include <iterator>
+#include <stdexcept>
+#include <type_traits>
+//
+#include "ilist_base.hh"
+
+namespace jj_ir::ilist_detail {
+//
+
+template <typename Type, bool Cond>
+using MakeConstType = std::conditional_t<Cond, std::add_const_t<Type>, Type>;
+
+/**
+ * @brief Iterator for intrusive lists  based on ilist_node.
+ *        Implementation without iterator traits
+ *        To check out correctness of INodeType tparam.
+ *
+ * @tparam INodeType,
+ * @tparam IsConst
+ * @tparam IsReverse
+ */
+template <typename INodeType, bool IsConst, bool IsReverse>
+class ilist_iterator final {
+public:
+    using value_type = MakeConstType<INodeType, IsConst>;
+    using pointer = value_type*;
+    using reference = value_type&;
+    using difference_type = std::ptrdiff_t;
+    using iterator_category = std::bidirectional_iterator_tag;
+    using node_pointer = MakeConstType<ilist_node, IsConst>*;
+
+private:
+    node_pointer m_ptr = nullptr;
+
+public:
+    ilist_iterator() = default;
+
+    /**
+     * @brief Construct from a ilist_node
+     */
+    explicit ilist_iterator(node_pointer node) : m_ptr(node) {}
+
+    explicit ilist_iterator(pointer pnode) : m_ptr(pnode) {}
+
+    explicit ilist_iterator(reference node_ref) : m_ptr(&node_ref) {}
+
+    //! TODO:  explicit conversion forward/reverse iterator
+    //! TODO:  const - cast
+    //! TOOD:  ctor a const iterator from a nonconst iterator
+
+    //! NOTE: Accessors
+
+    reference operator*() const noexcept {
+        return *static_cast<pointer>(m_ptr);
+    }
+
+    pointer operator->() const noexcept { return &operator*(); }
+
+    //! NOTE: Comparision
+    friend bool operator==(const ilist_iterator& lhs,
+                           const ilist_iterator rhs) noexcept {
+        return lhs.m_ptr == rhs.m_ptr;
+    }
+
+    friend bool operator!=(const ilist_iterator& lhs,
+                           const ilist_iterator rhs) noexcept {
+        return !(lhs == lhs);
+    }
+
+    //! NOTE: Increment, decrement operators
+    ilist_iterator& operator++() noexcept {
+        m_ptr = IsReverse ? m_ptr->get_prev() : m_ptr->get_next();
+        return *this;
+    }
+
+    ilist_iterator& operator--() noexcept {
+        m_ptr = IsReverse ? m_ptr->get_next() : m_ptr->get_prev();
+        return *this;
+    }
+
+    ilist_iterator operator--(int) {
+        auto tmp = *this;
+        --*this;
+        return tmp;
+    }
+    ilist_iterator operator++(int) {
+        auto tmp = *this;
+        ++*this;
+        return tmp;
+    }
+
+    node_pointer get_pnode() const noexcept {
+        return static_cast<node_pointer>(m_ptr);
+    }
+};
+
+}  // namespace jj_ir::ilist_detail
--- include/ir.hh
+++ include/ir.hh
@@ -1,13 +1,19 @@
 #ifndef INSTR_HH
 #define INSTR_HH
 
-#include "instrusive_list.hh"
+#include "intrusive_list/ilist.hh"
 #include "opcodes.hh"
+//
+#include <array>
 
 namespace jj_ir {
 
-enum class Type : uint8_t {
-    NONE,
+/**
+ * @brief Enum to describe instruction type
+ *
+ */
+enum class TypeId : uint8_t {
+    NONE = 0,
     I8,
     I16,
     I32,
@@ -17,54 +23,140 @@ enum class Type : uint8_t {
 class IRBuilder;
 class Instr;
 
-class BasicBlock final {
+class Type final {
+    TypeId m_id;
+
 public:
-    using value_type = IntrusiveList<Instr>::value_type;
+    Type(TypeId id = TypeId::NONE) : m_id(id) {}
+    TypeId get_type() const noexcept { return m_id; }
+};
+
+class Value {
+protected:
+    Type m_type = TypeId::NONE;
+
+public:
+    Value() = default;
+    Value(Type type) : m_type(type) {}
+    //
+    TypeId get_type() const noexcept { return m_type.get_type(); }
+};
+
+/**
+ * @brief Linear part of the code
+ *
+ */
+class BasicBlock final : public Value {// , public INode<BasicBlock> {
+public:
+    //! NOTE: Instruction iterators
+    // using iterator = InstListType::iterator;
+    // using const_iterator = InstListType::const_iterator;
+    // using reverse_iterator = InstListType::reverse_iterator;
+    // using const_reverse_iterator = InstListType::const_reverse_iterator;
+
+    static constexpr int MaxSucNum = 2;
 
 private:
     std::uint32_t m_bb_id = 0;
-    IntrusiveList<Instr> m_instrs;
+    //IntrusiveList<Instr> m_instrs;
     //
     Instr* m_first_instr = nullptr;
     Instr* m_last_instr = nullptr;
-
+    //
+    std::array<BasicBlock*, MaxSucNum> m_success;
+    // std::vector<BasicBlock*> m_predecess;
     //
     friend IRBuilder;
 
 public:
+    BasicBlock() = default;
     explicit BasicBlock(std::uint32_t bb_id) : m_bb_id(bb_id) {}
 
     auto bb_id() const noexcept { return m_bb_id; }
 
-    auto size() const noexcept { return m_instrs.size(); }
-
-    auto first_instr() const noexcept { return m_first_instr; }
-    auto last_instr() const noexcept { return m_last_instr; }
+    //auto size() const noexcept { return m_instrs.size(); }
+    //pointer first_instr() const noexcept { return m_first_instr; }
+    //pointer last_instr() const noexcept { return m_last_instr; }
 };
 
-class Instr : public INode<Instr> {
+class Instr : public Value {//, public INode<Instr> {
     //
-    uint32_t m_id = 0;
-    uint32_t m_bb_id = 0;
+    BasicBlock* m_parent = nullptr;
+
+protected:
     Opcode m_opcode = Opcode::NONE;
-    Type m_type = Type::NONE;
-    //
-    friend IRBuilder;
     //
 public:
     Instr() = default;
-    Instr(Type type) : m_type(type) {}
-
+    Instr(Type type) : Value{type} {}
+    Instr(Type type, BasicBlock* bb) : Value{type}, m_parent(bb) {}
+    //
     virtual ~Instr() = default;
     //
-    auto id() const noexcept { return m_id; }
-    auto bb_id() const noexcept { return m_bb_id; }
     auto opcode() const noexcept { return m_opcode; }
     auto type() const noexcept { return m_type; }
+    //
+    const BasicBlock* parent() const { return m_parent; }
+    BasicBlock* parent() { return m_parent; }
+    //    
+    friend IRBuilder;
+};
+
+class IfInstr final : public Instr {
+    //
+    BasicBlock* m_true = nullptr;
+    BasicBlock* m_false = nullptr;
+    //
+};
+
+class BinInstr final : public Instr {
+private:
+    // std::array<Value*, 2> m_inputs{};
+};
+
+class PhiInstr final : public Instr {
+    //
+};
+
+class CastInstr final : public Instr {
+    //
+};
+
+class Constant final : public Instr {
+    //
+};
+
+class CmpInstr final : public Instr {
+    //
 };
 
 class IRBuilder final {
     //
+private:
+    BasicBlock* m_bb;
+
+public:
+    /**
+     * @brief This specifies that created instructions should be appended to the
+     *        end of the specified block.
+     * @param[in] TheBB
+     */
+    void SetInsertPoint(BasicBlock* TheBB) {
+        // BB = TheBB;
+        // InsertPt = BB->end();
+    }
+
+    /**
+     * @brief This specifies that created instructions should be inserted before
+     *        the specified instruction.
+     * @param[in] I
+     */
+    void SetInsertPoint(Instr* I) {
+        // BB = I->getParent();
+        // InsertPt = I->getIterator();
+        // assert(InsertPt != BB->end() && "Can't read debug loc from end()");
+        // SetCurrentDebugLocation(I->getDebugLoc());
+    }
 };
 
 }  // namespace jj_ir
