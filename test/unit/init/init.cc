afbe6dc633981731764f4b4eec2a1a00fce3fd7b
uslstenn <shurygin0171@gmail.com>
2023-10-29 22:11:35 +0300

    [jj_ir] dir. hierarchy refactoring
    dir renamed test -> tests
    cmake copypaste deleted
    new initial unit tests added

--- test/unit/init/init.cc
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(init, test) {
-    constexpr int lhs = 1;
-    constexpr int rhs = 2;
-
-    ASSERT_EQ(lhs + rhs, 3);
-}
---
+++ tests/unit/intrusive_list/instrusive_list.cc
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+
+#include <cstdint>
+#include <memory>
+
+#include "intrusive_list/ilist.hh"
+#include "intrusive_list/ilist_base.hh"
+
+using namespace jj_ir::ilist_detail;
+
+class TestNode final : public jj_ir::ilist_detail::ilist_node {
+    int m_data{};
+
+public:
+    TestNode() = default;
+    explicit TestNode(int data) : m_data(data) {}
+
+    auto data() const noexcept { return m_data; }
+};
+
+// template <std::size_t size>
+class IListTest1 : public ::testing::Test {};
+
+TEST(IListTest1, empty) {
+    jj_ir::ilist<TestNode> ilist{};
+
+    EXPECT_TRUE(ilist.empty());
+    EXPECT_EQ(ilist.begin(), ilist.end());
+    EXPECT_EQ(ilist.size(), 0);
+}
+
+class IListTest2 : public ::testing::Test {
+protected:
+    IListTest2()
+        : node1(std::make_unique<TestNode>(1)),
+          node2(std::make_unique<TestNode>(2)) {}
+
+    std::unique_ptr<TestNode> node1;
+    std::unique_ptr<TestNode> node2;
+};
+
+TEST_F(IListTest2, push_back) {
+    jj_ir::ilist<TestNode> ilist{};
+    //
+    //
+    ilist.push_back(node1.release());
+    ilist.push_back(node2.release());
+
+    EXPECT_EQ(ilist.front().data(), 1);
+    EXPECT_EQ(ilist.back().data(), 2);
+}
