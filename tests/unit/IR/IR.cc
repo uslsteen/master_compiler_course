2c46806295092f12693d2d06c45cac8c1cf6109a
uslstenn <shurygin0171@gmail.com>
2023-10-31 15:03:37 +0300

    [jj_ir] IR test in progress

--- tests/unit/IR/IR.cc
+++ tests/unit/IR/IR.cc
@@ -1,4 +1,5 @@
 #include <cstdint>
+
 #include "gtest/gtest.h"
 //
 #include "IR/basic_block.hh"
@@ -7,6 +8,11 @@
 #include "IR/ir_builder.hh"
 //
 
+TEST(Function, init) {
+    //
+}
+
+//
 // Code:
 // int64_t fact(int32_t n) {
 //     int64_t res = 1;
@@ -41,10 +47,7 @@
 //     ret v5
 // }
 
-TEST(Function, init) {
-    // jj_ir::BasicBlock bb{1};
-    // jj_ir::IRBuilder builder{&bb};
-
+TEST(fib, init) {
     auto* fib_func = jj_ir::Function::create_function<jj_ir::Param>(
         jj_ir::Type::create<jj_ir::TypeId::I64>(), std::string{"fib"},
         jj_ir::Type::create<jj_ir::TypeId::I32>());
@@ -66,4 +69,33 @@ TEST(Function, init) {
         ASSERT_EQ(fib_func->front().bb_id(), 0);
         ASSERT_EQ(fib_func->back().bb_id(), 3);
     }
+    //
+    jj_ir::BasicBlock::link_blocks(bb1, bb0);
+    jj_ir::BasicBlock::link_blocks(bb2, bb1);
+    jj_ir::BasicBlock::link_blocks(bb3, bb1);
+
+    {
+        ASSERT_EQ(bb0->get_next(), bb1);
+        ASSERT_EQ(bb1->get_next(), bb2);
+        ASSERT_EQ(bb2->get_next(), bb3);
+        //
+        ASSERT_EQ(bb1->get_prev(), bb0);
+        ASSERT_EQ(bb2->get_prev(), bb1);
+        ASSERT_EQ(bb3->get_prev(), bb2);
+    }
+    //
+    jj_ir::IRBuilder builder{};
+    //
+    //! NOTE: initialization bb0 
+    builder.set_insert_point(bb0);
+    //
+    auto v1 = builder.create<jj_ir::ConstI32>(1);
+    auto v2 = builder.create<jj_ir::ConstI64>(2);
+    //
+    builer.create<jj_ir::BranchInstr>(bb1);
+
+    {
+        ASSERT_EQ(v1.);
+    }
+    //
 }
